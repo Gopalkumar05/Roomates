5afe626672b0f5a011dfb388b259f367bace919c
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-11-17 19:39:05 +0530

    Update AuthContext.jsx

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://roomates-k4tg.onrender.com';
 axios.defaults.baseURL = API_BASE_URL;
 const AuthContext = createContext();
 
