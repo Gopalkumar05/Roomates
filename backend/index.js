94c517b30af105bfc39050ca2b43306ba6edd319
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-11-17 19:29:38 +0530

    Uncomment and update server configuration

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,4 @@
 
-// import express from 'express'
-// import dotenv from 'dotenv'
-// import cors from 'cors'
-// import mongoose from 'mongoose';
-// import { createServer } from 'http';
-// import { Server } from 'socket.io';
-// import authRoutes from './routes/auth.js';
-// import expenseRoutes from './routes/expenses.js';
-// import roomRoutes from './routes/rooms.js';
-
-// const app = express();
-// dotenv.config()
-// const server = createServer(app);
-
-// app.use(express.json())
-// app.use(cors({
-//     origin: "http://localhost:5173"
-// }));
-
-// const io = new Server(server, { 
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//     credentials: true
-//   },
-//   path: "/socket.io/"
-// });
-
-// mongoose.connect(process.env.Mongodb).then(() => {
-//   console.log("Mongodb connected")
-// }).catch(() => {
-//   console.log("connection error")
-// })
-
-// io.on('connection', (socket) => {
-//   console.log('User connected:', socket.id);
-  
-//   socket.on('join-room', (roomId) => {
-//     socket.join(roomId);
-//     console.log(`User ${socket.id} joined room ${roomId}`);
-//   });
-  
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id);
-//   });
-// });
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/expenses', expenseRoutes);
-// app.use('/api/rooms', roomRoutes);
-
-
-// server.listen(process.env.Port, () => {
-//     console.log("Server Start on port " + process.env.Port);
-// });
-
-// export { io };
-
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -78,12 +20,12 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173","https://roomates-k4tg.onrender.com"]
 }));
 
 const io = new Server(server, { 
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://roomates-k4tg.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true
   },
